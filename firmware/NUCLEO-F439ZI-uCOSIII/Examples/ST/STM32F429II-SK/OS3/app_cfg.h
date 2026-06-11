28a2a1b6d5e7b4e228404b2eb0f9c5e880541a74
Nayeeun5 <dmsdpsk05@gmail.com>
2026-06-11 21:14:55 +0900

    Add sensor and joystick configuration to app_cfg.h
    Added configuration for sensors and joystick including distance thresholds and GPIO settings.

--- firmware/NUCLEO-F439ZI-uCOSIII/Examples/ST/STM32F429II-SK/OS3/app_cfg.h
+++ firmware/NUCLEO-F439ZI-uCOSIII/Examples/ST/STM32F429II-SK/OS3/app_cfg.h
@@ -40,6 +40,8 @@
 #ifndef  APP_CFG_MODULE_PRESENT
 #define  APP_CFG_MODULE_PRESENT
 
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_adc.h"
 
 /*
 *********************************************************************************************************
@@ -111,5 +113,43 @@
 
 #define  BSP_TRACE_INFO(x)               ((BSP_CFG_TRACE_LEVEL  >= TRACE_LEVEL_INFO) ? (void)(BSP_CFG_TRACE x) : (void)0)
 #define  BSP_TRACE_DBG(x)                ((BSP_CFG_TRACE_LEVEL  >= TRACE_LEVEL_DBG)  ? (void)(BSP_CFG_TRACE x) : (void)0)
+
+
+/*
+*********************************************************************************************************
+*                                      SENSOR / JOYSTICK CONFIG
+*********************************************************************************************************
+*/
+
+// 거리 기준값
+#define  DIST_WARN_CM                  30
+#define  DIST_CRITICAL_CM              15
+#define  SENSOR_TASK_PERIOD_MS         100u
+#define  ULTRA_TIMEOUT_US              30000u
+
+// HC-SR04 초음파 센서
+#define  ULTRA_PORT                    GPIOF
+#define  ULTRA_TRIG_PIN                GPIO_Pin_15
+#define  ULTRA_ECHO_PIN                GPIO_Pin_14
+
+// IR 장애물 센서
+#define  IR_PORT                       GPIOF
+#define  IR_PIN                        GPIO_Pin_13
+#define  IR_DETECTED_LEVEL             Bit_RESET
+
+// 조이스틱
+#define  JOY_ADC_PORT                  GPIOC
+#define  JOY_VRX_PIN                   GPIO_Pin_0
+#define  JOY_VRY_PIN                   GPIO_Pin_3
+#define  JOY_VRX_ADC_CHANNEL           ADC_Channel_10
+#define  JOY_VRY_ADC_CHANNEL           ADC_Channel_13
+
+/*
+ * 12-bit ADC 범위: 0 ~ 4095
+ * 중앙값은 보통 약 2048 근처.
+ */
+#define  JOY_ADC_LOW_TH                1400u
+#define  JOY_ADC_HIGH_TH               2600u
+#define  JOY_STABLE_COUNT              2u
 
 #endif
